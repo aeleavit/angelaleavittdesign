c500d45c17e63f0ac6fd83fdcba9cb1655869956
aeleavit <aeleavit@asu.edu>
2026-06-20 16:55:30 -0700

    Updated js form
    updated js

--- script.js
+++ script.js
@@ -16,110 +16,105 @@ function validateForm (e) {
     let firstNameInput = document.getElementById("firstName"); 
     let lastNameInput = document.getElementById("lastName");
     let commentsInput = document.getElementById("comments");
-    
     let phoneNumberInput = document.getElementById("phoneNumber");
     let emailInput = document.getElementById("email");
-    
-    // radio button
-    let radioButInput = document.getElementById("radioBut"); 
+
+    //radio buttons
+    let emailRadio = document.getElementById("pref-email");
+    let phoneNumberRadio = document.getElementById("pref-phone");
 
     //span display error message
     let firstNameErrorSpan = firstNameInput.previousElementSibling;
     let lastNameErrorSpan = lastNameInput.previousElementSibling;
     let commentsErrorSpan = commentsInput.previousElementSibling;
-
-
     let phoneNumberErrorSpan = phoneNumberInput.previousElementSibling;
     let emailErrorSpan = emailInput.previousElementSibling;
-    
-   // let radioButErrorSpan = commentsErrorInput.parentElementSibling; //radio button
-    
+    //radio button
+    let radioButError = document.getElementById("radioBut");
 
+    
     //paragraph to display error message to user
     let outputP = document.getElementById("defaultOutput");
 
     //clear out any past error messages and output
     firstNameErrorSpan.innerHTML = "";
     lastNameErrorSpan.innerHTML = "";
+    commentsErrorSpan.innerHTML = "";
     phoneNumberErrorSpan.innerHTML = "";
     emailErrorSpan.innerHTML = "";
-    commentsErrorSpan.innerHTML = "";
+    
+    //radio buttons
+    radioButError.innerHTML = "";
+    // paragraph
+    outputP.innerHTML = "";
 
-  //  outputP.innerHTML = "";
+  //  Start as true/valid
+    let isValid = true;
 
     if(firstNameInput.value === ""){
         firstNameErrorSpan.innerHTML = "Please enter your first name";
+        isValid = false;
+
     }
     
     if(lastNameInput.value === "") {
         lastNameErrorSpan.innerHTML = "Please enter your last name";
+        isValid = false;
     }
 
     if(commentsInput.value === "") {
         commentsErrorSpan.innerHTML = "Please enter your comment"
+        isValid = false;
+    }// if first input is not blank display message
+    
+    //radio buttons
+    if(!emailRadio.checked && !phoneNumberRadio.checked) {
+        radioButError.innerHTML = "Please enter your email or phone number";
+        isValid = false;
     }
 
-    
-
-    if(phoneNumberInput.value === "") {
-        phoneNumberErrorSpan.innerHTML = "Please enter your phone number or email";
+    // if user selects email
+    if(emailRadio.checked && emailInput.value === "") {
+        emailErrorSpan.innerHTML = "Please enter your email";
+        isValid = false;
     }
 
-    if(emailInput.value === "") {
-        emailErrorSpan.innerHTML = "Please enter your email or phone number";
-    } 
-    
-    
-    
-    
-    else{
-        // if first input is not blank display messager
+    // if user select phone
+    if(phoneNumberRadio.checked && phoneNumberInput.value ===""){
+        phoneNumberErrorSpan.innerHTML = "Please enter your phone number";
+        isValid = false;
+    }
+
+    if (isValid){
+        let contactInfo = {
+            firstName: firstNameInput.value,
+            lastName: lastNameInput.value,
+            comments: commentsInput.value, 
+            email: emailInput.value,
+            phoneNumber: phoneNumberInput.value
+        };
+   
         outputP.innerHTML = "Hello, " + firstNameInput.value + "! Thank you for your interest in Angela Leavitt's handbags!";
 
-    }
+    
+    
         // Clear out input
         firstNameInput.value = "";
         lastNameInput.value = "";
         phoneNumberInput.value = "";  
         emailInput.value = "";
         commentsInput.value = "";
-
-}
-
-//radio buttons
-
-let radio = document.querySelectorAll("#radioButtons input[type = 'radio']");
-
-//create an array
-
-let contact = [];
-
-//function
-function contactList(e) {
-
-// show picked choice comment TO DO ADD P
-let radioP = document.querySelector("#radioButtons p");
-
-//empty string
-let output = "";
-
-//which radio button was click
-let currentRadio = e.target;
-
-//True or False
-if(currentRadio.checked){
-    if(contact.indexOf(currentRadio.value) === -1) {
-        radio.push()
-
-    }
-
+ }
 }
 
 
 
 
 
-}
+
+
+
+
 
 
 
@@ -137,8 +132,3 @@ document.getElementById("darkMode").addEventListener("click", hideDarkMode);
 
 //Event Handler to validate form
 document.getElementById("submitBut").addEventListener("click", validateForm);
-
-//Event Handlers for radio buttons
-for (let radio of radiobuttons){
-        radio.addEventListener("change", contactList);
-}
