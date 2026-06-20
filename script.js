cedd4110dd17c4b0b3def592e25901f4f42bc572
aeleavit <aeleavit@asu.edu>
2026-06-19 20:05:14 -0700

    Dark Mode
    JS completed for dark mode. Also updated CSS

---
+++ script.js
@@ -0,0 +1,29 @@
+// Angela Leavitt website for handbags by Angela Leavitt
+
+// dark/light Mode
+
+"use strict"
+
+function hideDarkMode(){
+    document.querySelector("body").classList.toggle("darkMode");
+}
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+// Event Handlers dark mode
+document.getElementById("darkMode").addEventListener("click", hideDarkMode);
